865902c211779117305ce356aa7a9205b3cd6f34
emanuel.craciun <emanuel.craciun@savnet.ro>
2021-06-28 19:34:35 +0300

    partial2

--- app1.py
+++ app1.py
@@ -1,4 +1,8 @@
 # Some user does:
 from modul1 import func1
+print(80*"#")
+print(func1("Emanuel"))
 
-func1()
+print(80*"#")
+print(func1.__name__)
+print(func1.__doc__)
--- modul1.py
+++ modul1.py
@@ -1,27 +1,34 @@
 def conversation():
     def hello():
         print('Hello')
+
     return hello
 
+
 # Decorator
-def func1():
-    print('This does nothing')
+def func1(value):
+    """This function has some docstring"""
+    print(f'This does nothing but prints: {value}')
+    return "My func1 return"
+
 
 def decorator(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
+        """This function has some other docstring"""
         print('print before')
-        func()
+        result = func(*args, **kwargs)
         print('print after')
+        return result
+
     return wrapper
 
-func1 = decorator(func1)
 
+func1 = decorator(func1)
 
 if __name__ == "__main__":
     hello = conversation()
     print(type(hello))
     hello()
-
 
     result = decorator(func1)
     print(type(result))

3b70c61dd1e6418af1e5034e612f04e596a3844a
emanuel.craciun <emanuel.craciun@savnet.ro>
2021-06-30 18:32:54 +0300

    partial9

---
+++ C02_regex/app1_bookreader.py
@@ -0,0 +1,7 @@
+"""
+Create python class for object that can load contents of a text file and do:
+    - search for a user provided regular expression and return line number an text
+    - find all links in the book and return them
+    - get all chapters of the book and the first 3 lines of each chapter
+    - replace a section of text then open a temporary file for the user preview
+"""
---
+++ C02_regex/app2_cpuchecker.py
@@ -0,0 +1,5 @@
+"""Create a class for an object that can:
+ - continually monitor the cpu usage and send send alert when usage is over 50%
+ - get data about de cpu in dictionary format only for keys that have a value
+ -
+"""

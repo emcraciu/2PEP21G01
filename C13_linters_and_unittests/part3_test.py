4fe1c5cb58d5c9959dcee3881e97d095e22c0f76
emanuel.craciun <emanuel.craciun@savnet.ro>
2021-08-25 21:50:32 +0300

    modul 13 - updated

--- C13_linters_and_unittests/part3_test.py
+++ C13_linters_and_unittests/part3_test.py
@@ -1,15 +1,16 @@
+import json
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from C13_linters_and_unittests.part3 import time_getter
 
 output = ['Africa/Abidjan', 'Africa/Accra']
-output_json = '["Africa/Abidjan", "Africa/Accra"]'
+
 
 class TestTimeGetter(unittest.TestCase):
     @patch("C13_linters_and_unittests.part3.requests.get")
-    def test_content(self,get_mock):
-        get_mock.text = output_json
+    def test_content(self, get_mock):
+        get_mock.return_value = MagicMock(text=json.dumps(output))
         self.assertEqual(time_getter(), output)
 
 
